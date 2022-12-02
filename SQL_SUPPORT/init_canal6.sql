3289619a66cf05474b1bc51aa05a83b463dd00a3
Diego Balcazar <zero15_diego@hotmail.com>
2022-12-01 18:33:17 -0600

    archivo init agregado

---
+++ SQL_SUPPORT/init_canal6.sql
@@ -0,0 +1,14 @@
+select * from ACERVO;
+select * from COLECCION
+
+insert into ACERVO values ('MX-C6J','Fondo Canal 6 de Julio');
+insert into COLECCION values ('MX-C6J','Audiovisual');
+insert into SERIE values (1,'REGISTROS PROPIOS'), (1,'REGISTROS DE ARCHIVO');
+insert into SUBSERIE values (1,'MOVIMIENTOS SOCIALES'), (1,'ELECCIONES Y PARTIDOS POLÍTICOS')
+insert into GRUPO values (1,'EZLN'),(1,'Organizaciones armadas')
+insert into SUBGRUPO values (1,'Levantamiento de 1994'),(1,'Proceso de diálogo'),(1,'EZLN y sociedad Civil')
+insert into CONJUNTO values (1,'Caravanas'),(1,'Movilizaciones de EZLN')
+insert into SUBCONJUNTO values (1,'1994'), (1,'1996')
+
+
+
